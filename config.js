a03580d25c9762b7445d57970e9e84fe0b0cad18
10kgsea <86883729+10kgsea@users.noreply.github.com>
2021-07-03 21:44:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,14 +6,14 @@ const CONFIG = {
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'Thế à',
     title: 'Được rồi, giờ t có điều muốn nói',
-    desc: 'M có muốn nghe không? Nghe xong đừng bất ngờ nhé. ',
+    desc: 'M có muốn nghe không? Nghe xong đừng bất ngờ nhé',
     btnYes: 'Không, không nghe đâu',
     btnNo: 'Có chứ, nói đi',
     question:'Trên thế giới hơn 7 tỉ người, t chỉ muốn nói với mỗi m thế mà m lại không muốn nghe',
     btnReply: 'Gửi',
     reply: 'Không muốn nghe đấy, thì sao?',
     mess: 'Có phải m ghét t đúng không?',
-    messDesc: 'T biết mà. Là do t hay bắt nạt m nên m ghét t đúng không?.',
+    messDesc: 'T biết mà. Là do t hay bắt nạt m nên m ghét t đúng không?',
     btnAccept: 'Đúng đấy, sao nào?',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
